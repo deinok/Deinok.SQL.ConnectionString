e64e6f51ef3363ac272a31520c650c3d24b9a92a
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2016-11-29 13:42:43 +0100

    Agregar archivos de proyecto.

---
+++ src/Deinok.SQL.ConnectionString/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Deinok.SQL.ConnectionString
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+    }
+}
