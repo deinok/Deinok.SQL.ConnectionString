841310eb605a646dd5459435da117b1296016e94
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2016-11-29 13:51:30 +0100

    BASIC

---
+++ src/Deinok.SQL.ConnectionString/IConnectionString.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Deinok.SQL.ConnectionString{
+    public interface IConnectionString{
+    }
+}
