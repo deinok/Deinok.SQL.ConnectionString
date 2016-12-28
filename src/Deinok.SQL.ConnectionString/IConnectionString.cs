6f5af7920ab9428fbae4fb0a8b03cd60f7d4d727
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2016-12-28 06:28:35 +0100

    IConnectionStringSimplificated

--- src/Deinok.SQL.ConnectionString/IConnectionString.cs
+++ src/Deinok.SQL.ConnectionString/IConnectionString.cs
@@ -6,31 +6,6 @@
 	public interface IConnectionString {
 
 		/// <summary>
-		/// Name of the Database
-		/// </summary>
-		string Database { get; set; }
-
-		/// <summary>
-		/// Name of the Host
-		/// </summary>
-		string Host { get; set; }
-
-		/// <summary>
-		/// Port of the Host
-		/// </summary>
-		int? Port { get; set; }
-
-		/// <summary>
-		/// User of the Database
-		/// </summary>
-		string UserId { get; set; }
-
-		/// <summary>
-		/// Password of the User
-		/// </summary>
-		string Password { get; set; }
-
-		/// <summary>
 		/// Generate a ConnectionString ready for use
 		/// </summary>
 		/// <returns>The Connection String</returns>
