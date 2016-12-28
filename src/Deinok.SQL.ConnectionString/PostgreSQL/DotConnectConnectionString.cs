b062186e1b4db298a20c9ce04642190faac0712d
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2016-12-28 06:37:25 +0100

    DotConnect Added

---
+++ src/Deinok.SQL.ConnectionString/PostgreSQL/DotConnectConnectionString.cs
@@ -0,0 +1,40 @@
+﻿namespace Deinok.SQL.ConnectionString.PostgreSQL {
+
+	/// <summary>
+	/// https://www.connectionstrings.com/dotconnect-for-postgresql/
+	/// </summary>
+	public class DotConnectConnectionString : BaseConnectionString {
+
+		[ConnectionStringSerializeName("Host")]
+		public new string Host { get; set; }
+
+		[ConnectionStringSerializeName("Port")]
+		public new int? Port { get; set; }
+
+		[ConnectionStringSerializeName("Database")]
+		public new string Database { get; set; }
+
+		[ConnectionStringSerializeName("User ID")]
+		public new string UserId { get; set; }
+
+		[ConnectionStringSerializeName("Password")]
+		public new string Password { get; set; }
+
+		[ConnectionStringSerializeName("Pooling")]
+		public bool? Pooling { get; set; }
+
+		[ConnectionStringSerializeName("Min Pool Size")]
+		public int? MinPoolSize { get; set; }
+
+		[ConnectionStringSerializeName("Max Pool Size")]
+		public int? MaxPoolSize { get; set; }
+
+		[ConnectionStringSerializeName("Connection Lifetime")]
+		public int? ConnectionLifeTime { get; set; }
+
+		public override string GenerateConnectionString() {
+			return ConnectionStringSerializer.Serialize(this);
+		}
+
+	}
+}
---
+++ tests/Deinok.SQL.ConnectionString.Tests/PostgreSQL/DotConnectConnectionStringTest.cs
@@ -0,0 +1,28 @@
+﻿using Deinok.SQL.ConnectionString.PostgreSQL;
+using Xunit;
+
+namespace Deinok.SQL.ConnectionString.Tests.PostgreSQL {
+
+	public class DotConnectConnectionStringTest{
+
+		[Fact]
+		public void StandardTest() {
+			IConnectionString connectionString = new DotConnectConnectionString() {
+				Host = "localhost",
+				Port = 5432,
+				Database = "myDataBase",
+				UserId = "root",
+				Password = "myPassword"	,
+				Pooling=true,
+				MinPoolSize=0,
+				MaxPoolSize=100,
+				ConnectionLifeTime=0
+			};
+
+			string expected = "Host=localhost;Port=5432;Database=myDataBase;User ID=root;Password=myPassword;Pooling=true;Min Pool Size=0;Max Pool Size=100;Connection Lifetime=0;";
+			string actual = connectionString.GenerateConnectionString();
+			Assert.Equal(expected, actual);
+		}
+
+	}
+}
